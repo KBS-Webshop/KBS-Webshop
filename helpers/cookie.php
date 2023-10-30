6d934501ba52fed00d20f6c92aed8a0651b12815
Yam Yammington <85848371+YamYammington@users.noreply.github.com>
2023-10-30 15:49:51 +0100

    Functies US6

--- helpers/cookie.php
+++ helpers/cookie.php
@@ -1,4 +1,25 @@
 <?php
+
+if (isset($_POST['action'])) {
+    switch ($_POST['action']) {
+        case 'add':
+            addRowToCookie($_POST['StockItemID'], $_POST['StockItemName']);
+            break;
+        case 'remove':
+            removeRowFromCookie($_POST['StockItemID']);
+            break;
+        case 'increment':
+            incrementAmount($_POST['StockItemID']);
+            break;
+        case 'decrement':
+            decrementAmount($_POST['StockItemID']);
+            break;
+        case 'change_amt':
+            changeAmount($_POST['StockItemID'], $_POST['amount']);
+            break;
+    }
+}
+
 function addRowToCookie($id, $name) {
     if(isset($_COOKIE["basket"])) {
         $basket_rows=json_decode($_COOKIE["basket"], true);
@@ -14,3 +35,113 @@ function addRowToCookie($id, $name) {
 
     }
 }
+
+function removeRowFromCookie($id) {
+    if(isset($_COOKIE["basket"])) {
+        $basket = json_decode($_COOKIE["basket"], true);
+
+        # voor elk product, als het id in de cookie overeenkomt met de gegeven id, verwijder het
+        foreach ($basket as $key => $value) {
+            if ($value['product']['StockItemID'] == $id) {
+                unset($basket[$key]);
+                setcookie("basket", json_encode($basket), 2147483647);
+                return;
+            }
+        }
+    }
+}
+
+function changeAmount($id, $amount) {
+    if(isset($_COOKIE["basket"])) {
+        $basket = json_decode($_COOKIE["basket"], true);
+
+        # voor elk product, als het id in de cookie overeenkomt met de gegeven id, verander het aantal naar $amount
+        foreach ($basket as $key => $value) {
+            if ($value['product']['StockItemID'] == $id) {
+                $basket[$key]['amount'] = $amount;
+                setcookie("basket", json_encode($basket), 2147483647);
+                return;
+            }
+        }
+    }
+}
+
+# voor het geval dat naast een input box voor het aanpassen van het aantal
+# ook knoppen voor het verhogen en verlagen van het aantal worden gebruikt,
+# bestaan incrementAmount() en decrementAmount() voor gebruik in de knoppen
+function incrementAmount($id) {
+    if(isset($_COOKIE["basket"])) {
+        $basket = json_decode($_COOKIE["basket"], true);
+
+        # voor elk product, als het id in de cookie overeenkomt met de gegeven id, verhoog het aantal met 1
+        foreach ($basket as $key => $value) {
+            if ($value['product']['StockItemID'] == $id) {
+                $basket[$key]['amount'] += 1;
+                setcookie("basket", json_encode($basket), 2147483647);
+                return;
+            }
+        }
+    }
+}
+
+function decrementAmount($id) {
+    if(isset($_COOKIE["basket"])) {
+        $basket = json_decode($_COOKIE["basket"], true);
+
+        # voor elk product, als het id in de cookie overeenkomt met de gegeven id, verlaag het aantal met 1
+        foreach ($basket as $key => $value) {
+            if ($value['product']['StockItemID'] == $id) {
+                $basket[$key]['amount'] -= 1;
+                setcookie("basket", json_encode($basket), 2147483647);
+                return;
+            }
+        }
+    }
+}
+
+?>
+
+
+<!-- ALLES ONDER DEZE REGEL IS VOOR HET TESTEN VAN DE BOVENSTAANDE FUNCTIES -->
+
+<!--<div>-->
+<!--    <form method="post">-->
+<!--        <p>Dummy 1</p>-->
+<!--        <input type="hidden" name="StockItemID" value="1">-->
+<!--        <input type="hidden" name="StockItemName" value="Dummy 1">-->
+<!--        <input type="hidden" name="action" value="add">-->
+<!--        <label>Amount:-->
+<!--            <input type="number" value="1" name="amount" min="1" max="100">-->
+<!--        </label>-->
+<!--        <input type="submit" value="Add to basket">-->
+<!--    </form>-->
+<!---->
+<!--    <form method="post">-->
+<!--        <p>Dummy 2</p>-->
+<!--        <input type="hidden" name="StockItemID" value="2">-->
+<!--        <input type="hidden" name="StockItemName" value="Dummy 2">-->
+<!--        <input type="hidden" name="action" value="add">-->
+<!--        <label>Amount:-->
+<!--            <input type="number" value="1" name="amount" min="1" max="100">-->
+<!--        </label>-->
+<!--        <input type="submit" value="Add to basket">-->
+<!--    </form>-->
+<!--</div>-->
+<!---->
+<!--<p>--><?php //print("Cookie Basket is set to: " . $_COOKIE["basket"]); ?><!--</p>-->
+<!---->
+<!--<div>-->
+<!--    --><?php
+//
+//    $basket = json_decode($_COOKIE["basket"], true);
+//
+//    foreach ($basket as $key => $value) {
+//        print("<p>" . $value['product']['StockItemName'] . "</p>");
+//        print("<form method='post'>");
+//        print("<input type='hidden' name='StockItemID' value='" . $value['product']['StockItemID'] . "'>");
+//        print("<input type='hidden' name='action' value='remove'>");
+//        print("<input type='submit' value='Remove from basket'>");
+//        print("</form>");
+//    }
+//    ?>
+<!--</div>-->

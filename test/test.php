6cc59e73385637ce4edb728dbe3eed99d02498f3
Yam Yammington <85848371+YamYammington@users.noreply.github.com>
2023-11-23 10:36:15 +0100

    verbeteringetjes

--- test/test.php
+++ test/test.php
@@ -2,10 +2,7 @@
 # ALS ER GEEN ERRORS ZIJN IS DE ENIGE OUTPUT VAN DIT BESTAND 'Tests passed'.
 # ANDERS WORDT AANGEGEVEN WAT DE ERROR(S) IS/ZIJN.
 
-# class AssertExceptionCustom extends Exception {}
-
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+// class AssertExceptionCustom extends Exception {}
 
 # normale assert stopt de code, deze functie niet
 function assert_custom($assertion, $message = "") {
@@ -16,7 +13,7 @@ function assert_custom($assertion, $message = "") {
 
 
 # TESTS VOOR cookie.php
-# cookie.php moet handmatig getest worden
+# cookie.php moet handmatig getest worden, $_COOKIE werkt niet in CLI-modus
 function cookie_tests() {
     include __DIR__ . "/../helpers/cookie.php";
 
@@ -113,6 +110,7 @@ function database_test() {
         assert_custom($Connection != null, "connectToDatabase() in file database.php failed");
     } catch (Throwable $e) {
         echo($e->getMessage());
+        return;
     }
 
     # Test voor getHeaderStockGroups()
@@ -135,6 +133,9 @@ function database_test() {
     try {
         $StockItem = getStockItem(1, $Connection);
         assert_custom($StockItem != null, "getStockItem() in file database.php failed");
+
+        $StockItem = getStockItem(0, $Connection);
+        assert_custom($StockItem == null, "getStockItem() in file database.php failed");
     } catch (Throwable $e) {
         echo($e->getMessage());
     }
@@ -157,10 +158,10 @@ function utils_test() {
 
     # Test voor getVoorraadTekst()
     try {
-        $voorraadTekst = getVoorraadTekst(1001);
-        assert_custom($voorraadTekst == "Ruime voorraad beschikbaar.", "getVoorraadTekst() in file utils.php failed; 1001 items");
+        assert_custom(getVoorraadTekst(1001) == "Ruime voorraad beschikbaar.", "getVoorraadTekst() in file utils.php failed; 1001 items");
         assert_custom(getVoorraadTekst(1000) == "Voorraad: 1000", "getVoorraadTekst() in file utils.php failed; 1000 items");
-        assert_custom(getVoorraadTekst(999) == "Voorraad: 999", "getVoorraadTekst() in file utils.php failed; 999 items");
+        assert_custom(getVoorraadTekst(0) == "Voorraad: 0", "getVoorraadTekst() in file utils.php failed; 0 items");
+        assert_custom(getVoorraadTekst(-1) == "Voorraad: -1", "getVoorraadTekst() in file utils.php failed; -1 items");
     } catch (Throwable $e) {
         echo($e->getMessage());
     }
@@ -172,24 +173,40 @@ function utils_test() {
 
         $verkoopPrijs = berekenVerkoopPrijs(100, 9);
         assert_custom($verkoopPrijs == 109, "berekenVerkoopPrijs() in file utils.php failed; 9% BTW");
+
+        $verkoopPrijs = berekenVerkoopPrijs(100, 0);
+        assert_custom($verkoopPrijs == 100, "berekenVerkoopPrijs() in file utils.php failed; 0% BTW");
+
+        # // functie vindt dit niet zo leuk
+        # $verkoopPrijs = berekenVerkoopPrijs(100, -5);
+        # assert_custom($verkoopPrijs == 100, "berekenVerkoopPrijs() in file utils.php failed; -5% BTW");
     } catch (Throwable $e) {
         echo($e->getMessage());
     }
-
 }
 
 function run_tests() {
+    // logt alle errors
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+
+    // maakt gebruik van output buffering
     ob_start();
+
+    // verandert de working directory naar de root
     chdir(__DIR__ . "/../");
 
     try {
         database_test();
         utils_test();
-    } catch(Exception $ignored) {}
-    finally {
+
+    // finally voert uit onafhankelijk van wat er gebeurt
+    } finally {
+        // check of buffer leeg is
         if (!ob_get_contents()) {
             echo "Tests passed";
         }
+        // output buffer dumpen
         ob_end_flush();
     }
 }

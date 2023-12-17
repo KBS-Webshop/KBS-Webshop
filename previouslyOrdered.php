748e3234765731453b64044403e9f5749c18c092
Stijn2049 <144688923+Stijn2049@users.noreply.github.com>
2023-12-17 20:20:21 +0100

    previously ordered items rework

--- previouslyOrdered.php
+++ previouslyOrdered.php
@@ -1,46 +1,37 @@
 <?php
+// Include necessary files
 include __DIR__ . "/components/header.php";
 include __DIR__ . "/helpers/utils.php";
 ?>
-<h1>Eerder gekochte producten</h1>
+    <h1>Eerder gekochte producten</h1>
 <?php
 $previousOrders = getPreviouslyOrderedID($databaseConnection, $_SESSION["user"]["customer"]["CustomerID"]);
 if ($previousOrders != null) {
     foreach ($previousOrders as $orderID) {
         $orderlines = getPreviousOrderLines($databaseConnection, $orderID);
         ?>
-        <div class="previouslyOrderedItem">
-        <h2> order id: <?php print $orderID; ?> </h2>
-        <?php
-        foreach ($orderlines as $orderline) {
-            ?>
+        <div class="previouslyOrdered">
+            <h2> Order ID: <?php print $orderID; ?> </h2>
             <?php
-            $orderlineID = $orderline["OrderlineID"];
-            getPreviouslyBought($databaseConnection, $orderlineID);
-            $bestelDatum = explode(" ", $_SESSION["user"]["currentOrder"]["LastEditedWhen"]);
-            $bestelDatum = $bestelDatum[0];
-            getStockItemInfo($databaseConnection, $_SESSION["user"]["currentOrder"]["StockItemID"]);
-            $StockItemImage = getStockItemImage($_SESSION["user"]["currentOrder"]["StockItemID"], $databaseConnection);
-    ?>
+            foreach ($orderlines as $orderline) {
+                $orderlineID = $orderline["OrderlineID"];
+                getPreviouslyBought($databaseConnection, $orderlineID);
+                $bestelDatum = explode(" ", $_SESSION["user"]["currentOrder"]["LastEditedWhen"]);
+                $bestelDatum = $bestelDatum[0];
+                getStockItemInfo($databaseConnection, $_SESSION["user"]["currentOrder"]["StockItemID"]);
+                $StockItemImage = getStockItemImage($_SESSION["user"]["currentOrder"]["StockItemID"], $databaseConnection);
+                ?>
                 <div class="previouslyOrderedItem">
-                    <div class="ImgFrame"
-                         style="background-image: url('<?php print "Public/StockItemIMG/" . $StockItemImage[0]["ImagePath"]; ?>'); background-size: contain; background-repeat: no-repeat; background-position: center;">
+                    <div class="ImgFrame" style="background-image: url('<?php print "Public/StockItemIMG/" . $StockItemImage[0]["ImagePath"]; ?>');"></div>
+                    <div class="details">
+                        <h2> Naam: <?php print ($_SESSION["user"]["currentStockItem"]["StockItemName"]); ?> </h2>
+                        <p> Quantity: <?php print ($_SESSION["user"]["currentOrder"]["Quantity"]); ?> </p>
+                        <p> Besteldatum: <?php print ($bestelDatum); ?> </p>
                     </div>
                 </div>
-                <div class="previouslyOrderedItem">
-        <h2> naam: <?php print ($_SESSION["user"]["currentStockItem"]["StockItemName"]); ?> </h2>
-            </div>
-            <div class="previouslyOrderedItem">
-        <h2> quantity: <?php print ($_SESSION["user"]["currentOrder"]["Quantity"]); ?> </h2>
-            </div>
-            <div class="previouslyOrderedItem">
-        <h2> besteldatum: <?php print ($bestelDatum); ?> </h2>
-            </div>
-
-
-    <?php
-    }
-?>
+                <?php
+            }
+            ?>
         </div>
         <?php
     }
@@ -48,7 +39,6 @@ if ($previousOrders != null) {
     print("U heeft nog geen bestellingen geplaatst.");
 }
 ?>
-
 <?php
-include __DIR__ . "/components/footer.php"
+include __DIR__ . "/components/footer.php";
 ?>

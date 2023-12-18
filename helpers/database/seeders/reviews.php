f8ecd3971460b5483bdabd406112f5c0ad4442e5
GerritScholten <146338174+GerritScholten@users.noreply.github.com>
2023-12-18 13:14:27 +0100

    voor de reviews

---
+++ helpers/database/seeders/reviews.php
@@ -0,0 +1,43 @@
+<?php
+include __DIR__ . "/helpers/database/reviewsDB.php";
+
+$conn = connectToDatabase();
+for ($i = 0; $i < 250; $i++) {
+    $personID = rand(2, 43);
+    $stockItemID = rand(1, 135);
+    $review = '';
+    $randNum = rand(1, 5);
+
+    switch ($randNum) {
+        case 1:
+            $review = 'Terrible purchase. Regret buying it.';
+            break;
+        case 2:
+            $review = 'Not satisfied. Poor quality and performance.';
+            break;
+        case 3:
+            $review = 'Average product. Does the job but nothing special.';
+            break;
+        case 4:
+            $review = 'Great purchase! Fantastic features and design.';
+            break;
+        case 5:
+            $review = 'Outstanding product. Exceeded my expectations!';
+            break;
+    }
+
+    $publicationDate = date('Y-m-d', strtotime('-' . rand(1, 365) . ' days'));
+    $rating = rand(1, 5);
+
+    $sql = "INSERT INTO nerdygadgets.Reviews (PersonID, StockItemID, review, publicationDate, rating)
+            VALUES ('$personID', '$stockItemID', '$review', '$publicationDate', '$rating')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Record inserted successfully.<br>";
+    } else {
+        echo "Error inserting record: " . $conn->error . "<br>";
+    }
+}
+
+$conn->close();
+?>
--- view.php
+++ view.php
@@ -150,8 +150,9 @@ $AlsoBought = getAlsoBought($_GET['id'], $databaseConnection);
                     $personID =  $_SESSION['user']['PersonID']; // hier moet een check komen op wie ingelogd is✅//
                     $existingPersonIDs = array_column(getPersonIDs($StockItem['StockItemID'], $databaseConnection), 'PersonID');
                     $gekochteItemsPersoon = didUserBuy($personID, $databaseConnection);
+                    print_r( $gekochteItemsPersoon);
                     if (!in_array($personID, $existingPersonIDs)
-                        && $_SESSION['user']['isLoggedIn']==1
+                        && $_SESSION['user']['isLoggedIn'] == 1
                         && in_array($StockItem['StockItemID'], $gekochteItemsPersoon))
                     { //&& de persoon het product heeft gekocht✅
                         ?>
@@ -323,50 +324,7 @@ $AlsoBought = getAlsoBought($_GET['id'], $databaseConnection);
     }
     ?>
 </div>
-</div>
-<?php
-//$conn = connectToDatabase();
-//
-//// Loop to generate and execute INSERT statements
-//for ($i = 0; $i < 50; $i++) {
-//    $personID = rand(2, 43);
-//    $stockItemID = rand(1, 135);
-//    $review = '';
-//    $randNum = rand(1, 5);
-//
-//    switch ($randNum) {
-//        case 1:
-//            $review = 'Terrible purchase. Regret buying it.';
-//            break;
-//        case 2:
-//            $review = 'Not satisfied. Poor quality and performance.';
-//            break;
-//        case 3:
-//            $review = 'Average product. Does the job but nothing special.';
-//            break;
-//        case 4:
-//            $review = 'Great purchase! Fantastic features and design.';
-//            break;
-//        case 5:
-//            $review = 'Outstanding product. Exceeded my expectations!';
-//            break;
-//    }
-//
-//    $publicationDate = date('Y-m-d', strtotime('-' . rand(1, 365) . ' days'));
-//    $rating = rand(1, 5);
-//
-//    $sql = "INSERT INTO nerdygadgets.Reviews (PersonID, StockItemID, review, publicationDate, rating)
-//            VALUES ('$personID', '$stockItemID', '$review', '$publicationDate', '$rating')";
-//
-//    if ($conn->query($sql) === TRUE) {
-//        echo "Record inserted successfully.<br>";
-//    } else {
-//        echo "Error inserting record: " . $conn->error . "<br>";
-//    }
-//}
-//
-//// Close connection
-//$conn->close();
-//?>
+
+
 
 

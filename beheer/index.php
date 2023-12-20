4a00879cc73ee353cba602cd44a1f418a503ff77
Stijn2049 <144688923+Stijn2049@users.noreply.github.com>
2023-12-20 10:07:06 +0100

    buttons beheer

--- beheer/index.php
+++ beheer/index.php
@@ -4,7 +4,12 @@ include "../helpers/utils.php";
 ?>
 <h1>Beheer pagina</h1>
 <a href="deal.php">deal pagina</a> <br>
-<a href="loyalty.php">loyalty pagina</a>
+<a href="loyalty.php">loyalty pagina</a> <br>
+<a href="discount.php">Discount pagina</a> <br>
+<a href="loyalty-configuration.php">loyalty configuration pagina</a> <br>
+<a href="mail_aanpas_keuze.php">mail aanpas keuze pagina</a> <br>
+<a href="mail_beheer.php">mail beheer pagina</a> <br>
+<a href="productOnIndex.php">product on index pagina</a> <br>
 <?php
 include "../components/footer.php"
 ?>
